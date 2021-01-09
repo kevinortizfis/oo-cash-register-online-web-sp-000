1e19bd0c7ff8ffacca14907fd40d8fcdec280ea8
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2021-01-09 21:25:29 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,8 +16,11 @@ def add_item(title,price,quantity = 1)
 end
 
 def apply_discount
+  if discount !=0
   @total = @total*(100-discount)/100
   "After the discount, the total comes to $#{@total}."
+else "There is no discount to apply."
+end
 end
 
 def items
