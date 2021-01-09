31751b12065fb421c8187d9953b77c7ae3958165
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2021-01-09 21:38:30 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,7 +14,7 @@ end
 def add_item(title,price,quantity = 1)
   # total << title,(price * quantity)
   @total = @total + price*quantity
-  @ITEMS << title * quantity
+  @ITEMS << title
 end
 
 def apply_discount
