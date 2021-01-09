121bfac7cd65eb384b844c27f5cc9f72cab787a0
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2021-01-09 21:33:15 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,5 +1,5 @@
 class CashRegister
-attr_accessor :total, :discount
+attr_accessor :total, :discount, :items
 
 def initialize(discount = 0)
   @total = 0
@@ -24,6 +24,7 @@ def apply_discount
 end
 
 def items
+
 end
 
 def void_last_transaction
