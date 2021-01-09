81063358a1f0a2b94401895623cd1d7d7c52e71a
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2021-01-09 20:37:35 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 class CashRegister
 attr_accessor :total, :discount,
 
-def initialize(total = 0,employee_discount = 0)
+def initialize(total = 0,discount = 0)
   @total = total
   @employee_discount = employee_discount
 end
